68c2eee07a37cd9ccba46ddc2b493b3ed87b05ab
Walter Gutierrez <elterrible8596@hotmail.com>
2022-01-09 21:01:01 -0400

    Project Re-vamp

---
+++ script/modal.js
@@ -0,0 +1,34 @@
+//Modal
+
+if(document.getElementById("btnModal")){
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("btnModal");
+    var span = document.getElementsByClassName("close")[0];
+    var body = document.getElementsByTagName("body")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+
+        body.style.position = "static";
+        body.style.height = "100%";
+        body.style.overflow = "hidden";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+
+        body.style.position = "inherit";
+        body.style.height = "auto";
+        body.style.overflow = "visible";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+
+            body.style.position = "inherit";
+            body.style.height = "auto";
+            body.style.overflow = "visible";
+        }
+    }
+}
